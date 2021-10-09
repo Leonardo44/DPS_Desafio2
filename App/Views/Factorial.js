03f380825d446275ccbad1b5cea5d273faffaea8
leonarrdo404 <leo@ese.plus>
2021-10-09 17:28:14 -0600

    factorial view

--- App/Views/Factorial.js
+++ App/Views/Factorial.js
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Button, StyleSheet, Alert, TouchableOpacity } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import Separator from "../Components/Separator";
+import { TextInput } from "react-native-gesture-handler";
 import ButtonStyle from "../styles/ButtonStyle";
+import FormStyle from "../styles/FormStyle";
+import Separator from "../Components/Separator";
 
 const Factorial = ({ navigation }) => {
+    const [nOne, onChangeNOne] = useState();
+    const [nResult, setNResult] = useState();
+
+    const calculate = e => {
+        if (isNaN(nOne) || nOne === undefined || nOne < 0) {
+            Alert.alert('Ingrese valores enteros positivos');
+        } else {
+            if (Number.isInteger(parseFloat(nOne))) {
+                let result = factorialize(nOne)
+                setNResult(`Respuesta: ${result}`);
+            } else {
+                Alert.alert('Ingrese valores enteros positivos');
+            }
+        }
+    };
+
+    const factorialize = num => {
+        if (num < 0) 
+              return -1;
+        else if (num == 0) 
+            return 1;
+        else {
+            return (num * factorialize(num - 1));
+        }
+    };
+    
     return (
         <ScrollView>
+            <View style={FormStyle.container}>
+                <Text></Text>
+                <View>
+                    <Text style={FormStyle.textInput}>Número</Text>
+                    <TextInput
+                        style={FormStyle.input}
+                        onChangeText={onChangeNOne}
+                        value={nOne}
+                        keyboardType="numbers-and-punctuation"
+                    ></TextInput>
+                </View>
+                <Separator />
+                <TouchableOpacity
+                    style={ButtonStyle.appButtonContainer}
+                    onPress={() => calculate()}
+                >
+                    <Text style={ButtonStyle.appButtonText}>Calcular</Text>
+                </TouchableOpacity>
 
+                <View style={FormStyle.responseView}>
+                    <Text style={FormStyle.responseText}>{nResult}</Text>
+                </View>
+            </View>
         </ScrollView>
     );
 };
